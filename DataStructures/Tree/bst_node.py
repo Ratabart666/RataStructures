efff79887054752a758b438c64a8d0c8f4c9a1bb
Ratabart666 <t.gomezs2@uniandes.edu.co>
2025-07-10 20:08:20 -0500

    Refactor bst_node: Replace explicit None checks with Pythonic conditionals
    Changed all 'if x is None' to 'if not x' and 'if x is not None' to 'if x' throughout the module. This follows Python truthiness conventions and PEP 8 guidelines for more idiomatic code.

--- DataStructures/Tree/bst_node.py
+++ DataStructures/Tree/bst_node.py
@@ -7,17 +7,17 @@ def new_node(key, value):
 
 
 def get_value(my_node):
-    value = my_node['value'] if my_node is not None else None
+    value = my_node['value'] if my_node else None
     return value
 
 
 def get_key(my_node):
-    key = my_node['key'] if my_node is not None else None
+    key = my_node['key'] if my_node else None
     return key
 
 
 def insert_node(root, key, value):
-    if root is None:
+    if not root:
         root = new_node(key, value)
     elif key == get_key(root):
         root['value'] = value
@@ -35,7 +35,7 @@ def insert_node(root, key, value):
 
 
 def get_node(root, key):
-    if root is None:
+    if not root:
         value = None
     elif key == get_key(root):
         value = get_value(root)
@@ -47,7 +47,7 @@ def get_node(root, key):
 
 
 def remove_node(root, key):
-    if root is not None:
+    if root:
         if key < get_key(root):
             old_size = size_tree(root['left'])
             root['left'] = remove_node(root['left'], key)
@@ -59,13 +59,13 @@ def remove_node(root, key):
             new_size = size_tree(root['right'])
             root['size'] -= old_size - new_size
         elif key == get_key(root):
-            if root['left'] is None and root['right'] is None:
+            if not root['left'] and not root['right']:
                 root = None
-            elif root['left'] is None:
+            elif not root['left']:
                 root = root['right']
-            elif root['right'] is None:
+            elif not root['right']:
                 root = root['left']
-            else:
+            elif root['left'] and root['right']:
                 succesor_key = get_min_node(root['right'])
                 succesor_value = get_node(root['right'], succesor_key)
                 root['right'] = delete_min_tree(root['right'])
@@ -76,12 +76,12 @@ def remove_node(root, key):
 
 
 def size_tree(root):
-    size = root['size'] if root is not None else 0
+    size = root['size'] if root else 0
     return size
 
 
 def key_set_tree(root, key_list):
-    if root is not None:
+    if root:
         key_list = key_set_tree(root['left'], key_list)
         key_list = lt.add_last(key_list, get_key(root))
         key_list = key_set_tree(root['right'], key_list)
@@ -89,7 +89,7 @@ def key_set_tree(root, key_list):
 
 
 def value_set_tree(root, value_list):
-    if root is not None:
+    if root:
         value_list = value_set_tree(root['left'], value_list)
         value_list = lt.add_last(value_list, get_value(root))
         value_list = value_set_tree(root['right'], value_list)
@@ -97,28 +97,28 @@ def value_set_tree(root, value_list):
 
 
 def get_min_node(root):
-    if root is None:
+    if not root:
         key = None
-    elif root['left'] is None:
+    elif not root['left']:
         key = get_key(root)
-    elif root['left'] is not None:
+    elif root['left']:
         key = get_min_node(root['left'])
     return key
 
 
 def get_max_node(root):
-    if root is None:
+    if not root:
         key = None
-    elif root['right'] is None:
+    elif not root['right']:
         key = get_key(root)
-    elif root['right'] is not None:
+    elif root['right']:
         key = get_max_node(root['right'])
     return key
 
 
 def delete_min_tree(root):
-    if root is not None:
-        if root['left'] is None:
+    if root:
+        if not root['left']:
             root = root['right']
         else:
             root['left'] = delete_min_tree(root['left'])
@@ -127,66 +127,66 @@ def delete_min_tree(root):
 
 
 def delete_max_tree(root):
-    if root is not None:
-        if root['right'] is None:
+    if root:
+        if not root['right']:
             root = root['left']
-        elif root['right'] is not None:
+        elif root['right']:
             root['right'] = delete_max_tree(root['right'])
             root['size'] -= 1
     return root
 
 
 def floor_key(root, key):
-    if root is None:
+    if not root:
         floor = None
-    elif root is not None:
+    elif root:
         if key == get_key(root):
             floor = get_key(root)
         elif key < get_key(root):
             floor = floor_key(root['left'], key)
         elif key > get_key(root):
-            if root['right'] is None or get_key(root['right']) > key:
+            if not root['right'] or get_key(root['right']) > key:
                 floor = get_key(root)
-            if root['right'] is not None and get_key(root['right']) < key:
+            if root['right'] and get_key(root['right']) < key:
                 floor = floor_key(root['right'], key)
     return floor
 
 
 def ceiling_key(root, key):
-    if root is None:
+    if not root:
         ceiling = None
-    else:
+    elif root:
         if key == get_key(root):
             ceiling = get_key(root)
         elif key > get_key(root):
             ceiling = ceiling_key(root['right'], key)
         elif key < get_key(root):
-            if root['left'] is None or get_key(root['left']) < key:
+            if not root['left'] or get_key(root['left']) < key:
                 ceiling = get_key(root)
-            else:
+            elif root['left'] and get_key(root['left']) > key:
                 ceiling = ceiling_key(root['left'], key)
     return ceiling
 
 
 def rank_keys(root, key):
-    if root is None:
+    if not root:
         rank = 0
-    else:
+    elif root:
         if key < get_key(root):
             rank = rank_keys(root['left'], key)
         elif key > get_key(root):
             rank_left = size_tree(root['left'])
             rank_right = rank_keys(root['right'], key)
             rank = rank_left + 1 + rank_right
-        else:
+        elif key == get_key(root):
             rank = size_tree(root['left'])
     return rank
 
 
 def select_key(root, pos):
-    if root is None:
+    if not root:
         key = None
-    elif root is not None:
+    elif root:
         left_size = size_tree(root['left'])
         if pos == left_size:
             key = get_key(root)
@@ -198,16 +198,16 @@ def select_key(root, pos):
 
 
 def height_tree(root):
-    if root is None:
-        return 0
-    else:
+    if not root:
+        return -1
+    elif root:
         left_height = height_tree(root['left'])
         right_height = height_tree(root['right'])
         return 1 + max(left_height, right_height)
 
 
 def keys_range(root, key_initial, key_final, key_list):
-    if root is not None:
+    if root:
         current_key = get_key(root)
         if key_initial < current_key:
             key_list = keys_range(
@@ -221,7 +221,7 @@ def keys_range(root, key_initial, key_final, key_list):
 
 
 def values_range(root, key_initial, key_final, value_list):
-    if root is not None:
+    if root:
         current_key = get_key(root)
         if key_initial < current_key:
             value_list = values_range(
@@ -235,7 +235,7 @@ def values_range(root, key_initial, key_final, value_list):
 
 
 def inorder_tree(root, node_list):
-    if root is not None:
+    if root:
         node_list = inorder_tree(root['left'], node_list)
         node_list = lt.add_last(node_list, root)
         node_list = inorder_tree(root['right'], node_list)
@@ -243,7 +243,7 @@ def inorder_tree(root, node_list):
 
 
 def preorder_tree(root, node_list):
-    if root is not None:
+    if root:
         node_list = lt.add_last(node_list, root)
         node_list = preorder_tree(root['left'], node_list)
         node_list = preorder_tree(root['right'], node_list)
@@ -251,7 +251,7 @@ def preorder_tree(root, node_list):
 
 
 def postorder_tree(root, node_list):
-    if root is not None:
+    if root:
         node_list = postorder_tree(root['left'], node_list)
         node_list = postorder_tree(root['right'], node_list)
         node_list = lt.add_last(node_list, root)
