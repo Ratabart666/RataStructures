a024efd76152f3d1a0607b60d3d34ba6706d0c82
Ratabart666 <t.gomezs2@uniandes.edu.co>
2025-07-10 20:42:07 -0500

    Add tree_traversal: Implement flexible BST traversal algorithms
    Added inorder, preorder, and postorder traversal functions with optional parameters for values extraction and key range filtering. Includes intelligent subtree pruning for optimized range queries.

--- DataStructures/Tree/tree_trasversal.py
+++ DataStructures/Tree/tree_trasversal.py
@@ -2,22 +2,58 @@ import DataStructures.List.single_linked_list as lt
 import DataStructures.Tree.bst_node as bst
 
 
-def inorder(my_order_map):
-    node_list = lt.new_list()
-    if my_order_map is not None and my_order_map['root'] is not None:
-        node_list = bst.inorder_tree(my_order_map['root'], node_list)
+def inorder_tree(root, node_list, values=False, key_initial=None, key_final=None):
+    if root:
+        current_key = bst.get_key(root)
+
+        if not key_initial or current_key > key_initial:
+            node_list = inorder_tree(
+                root['left'], node_list, values, key_initial, key_final)
+
+        if (not key_initial or current_key >= key_initial) and (not key_final or current_key <= key_final):
+            item = bst.get_value(root) if values else bst.get_key(root)
+            node_list = lt.add_last(node_list, item)
+
+        if not key_final or current_key < key_final:
+            node_list = inorder_tree(
+                root['right'], node_list, values, key_initial, key_final)
+
     return node_list
 
 
-def preorder(my_order_map):
-    node_list = lt.new_list()
-    if my_order_map is not None and my_order_map['root'] is not None:
-        node_list = bst.preorder_tree(my_order_map['root'], node_list)
+def preorder_tree(root, node_list, values=False, key_initial=None, key_final=None):
+    if root:
+        current_key = bst.get_key(root)
+
+        if (not key_initial or current_key >= key_initial) and (not key_final or current_key <= key_final):
+            item = bst.get_value(root) if values else bst.get_key(root)
+            node_list = lt.add_last(node_list, item)
+
+        if not key_initial or current_key > key_initial:
+            node_list = preorder_tree(
+                root['left'], node_list, values, key_initial, key_final)
+
+        if not key_final or current_key < key_final:
+            node_list = preorder_tree(
+                root['right'], node_list, values, key_initial, key_final)
+
     return node_list
 
 
-def postorder(my_order_map):
-    node_list = lt.new_list()
-    if my_order_map is not None and my_order_map['root'] is not None:
-        node_list = bst.postorder_tree(my_order_map['root'], node_list)
+def postorder_tree(root, node_list, values=False, key_initial=None, key_final=None):
+    if root:
+        current_key = bst.get_key(root)
+
+        if not key_initial or current_key > key_initial:
+            node_list = postorder_tree(
+                root['left'], node_list, values, key_initial, key_final)
+
+        if not key_final or current_key < key_final:
+            node_list = postorder_tree(
+                root['right'], node_list, values, key_initial, key_final)
+
+        if (not key_initial or current_key >= key_initial) and (not key_final or current_key <= key_final):
+            item = bst.get_value(root) if values else bst.get_key(root)
+            node_list = lt.add_last(node_list, item)
+
     return node_list
